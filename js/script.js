536e9874a0c0476a09abc3f57875e245520bf5b3
Julio <fernandeslimajulio@gmail.com>
2026-08-26 16:02:30 -0300

    comitar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 alert ('oi Betinha');
+
+
+var num = parseFloat(prompt("Digite um valor betinha:"));
+
+if (num == 0){
+    alert(`${num} é zero`);}
+
+else if (num %2 != 0){
+    alert(`${num} é impar`);
+}
+
+else{
+    alert(`${num} é par`);
+}
