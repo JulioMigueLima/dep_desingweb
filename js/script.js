7ee3a0c037c8425fcdf1555a613e1893e2640176
Julio <fernandeslimajulio@gmail.com>
2026-09-02 15:32:00 -0300

    Escopo de Hoisting

--- js/script.js
+++ js/script.js
@@ -9,11 +9,28 @@ let altura;
 function calcIdade (ano = 2026) {
   let idade=ano - nasc;
   console.log (`Idade dentro da funcao: ${idade}`);
-  return idade;
+  let menor;
+  if(idade < 18 ){
+    menor= true
+  }
 
-;}
+  else{
+    menor=false
+  }
 
-alert (`Idadde fora da função: ${calcIdade()}`);
+  return {idade, menor};
+}
+
+let retorno= calcIdade()
+
+if(retorno[1]){
+    alert(`Idade: ${retorno[0]}. Voce é menor de idade` )
+}
+else{
+    alert(`Idade: ${retorno[0]}. Voce é maior de idade` )
+}
+
+alert (`Idade fora da função: ${calcIdade()}`);
 
 /* console.log(`Idade fora da funcao: ${idade}`); 
              vai da erro
---
+++ js/script2.js
@@ -0,0 +1,14 @@
+/*Ex: diferença de "Let", "Var", "Const" */
+
+console.log(x);
+var x =10;
+x=20;
+console.log(x);
+
+
+let y=10;
+y=20;
+console.log(y);
+
+const z=20;
+console.log(z);
